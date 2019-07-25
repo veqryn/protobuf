8b67e295f4bdfca08831122832214ea78da6e65b
Chris Duncan <veqryn@hotmail.com>
2019-07-24 18:46:24 -0600

    add well-known-type helper

--- ptypes/gogenerate.go
+++ ptypes/gogenerate.go
@@ -3,4 +3,4 @@ package ptypes
 // To run from project root directory: `go generate -x ./ptypes/...`
 
 // Generate golang protobuf/grpc/gateway and swagger docs
-//go:generate protoc -I=/usr/local/include -I=./timestamp --go_out=plugins=grpc:${GOPATH}/src ./timestamp/timestamp.proto
+//go:generate protoc -I=/usr/local/include -I=${GOPATH}/src/github.com/veqryn/protobuf/ptypes/timestamp --go_out=plugins=grpc:${GOPATH}/src ${GOPATH}/src/github.com/veqryn/protobuf/ptypes/timestamp/timestamp.proto
